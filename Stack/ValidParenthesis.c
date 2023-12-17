c31f6dfca681c6fd490e38620a2df813e830c5da
Priyanka-1Arora <priyankaarora1728@gmail.com>
2023-12-17 13:32:30 +0530

    Added Valid Parenthesis

---
+++ Stack/ValidParenthesis.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+
+bool validParenthesis(char* s){
+    char arr[strlen(s)];
+    int top=-1;
+    for(int i=0;i<strlen(s);i++){
+        if((s[i]==')' && top==-1) || (s[i]==')' && arr[top]!='(')){
+            return false;
+        }
+        if((s[i]==']' && top==-1) || (s[i]==']' && arr[top]!='[')){
+            return false;
+        }
+        if((s[i]=='}' && top==-1) || (s[i]=='}' && arr[top]!='{')){
+            return false;
+        }
+        if(s[i]==')' && arr[top]=='('){
+            top-=1;
+        }
+        if(s[i]==']' && arr[top]=='['){
+            top-=1;
+        }
+        if(s[i]=='}' && arr[top]=='{'){
+            top-=1;
+        }
+        if(s[i]=='[' || s[i]=='{' || s[i]=='('){
+            top+=1;
+            arr[top]=s[i];
+        }
+    }
+    return top==-1?true:false;
+}
+int main(){
+    assert(validParenthesis("([{}])")==true);
+    assert(validParenthesis("({])")==false);
+    assert(validParenthesis("([{}]){")==false);
+}
